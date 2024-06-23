7d445724bf2c571ca914046b9e39ed53fa8c37d3
beltrandes <beltsmarques@hotmail.com>
2024-06-23 10:39:34 -0300

    chore: jpa dependency added, docker database configured, database connection and environment variables configured

--- src/main/java/com/gestone/gestone_api/GestoneApiApplication.java
+++ src/main/java/com/gestone/gestone_api/GestoneApiApplication.java
@@ -3,10 +3,16 @@ package com.gestone.gestone_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class GestoneApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(GestoneApiApplication.class, args);
 	}
 
