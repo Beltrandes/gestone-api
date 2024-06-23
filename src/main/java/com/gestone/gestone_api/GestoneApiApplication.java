4443ec3f9f4f855abc798b6744c85eed1c9bacbb
Beltrandes Marques <beltsmarques@hotmail.com>
2024-06-22 21:20:55 -0300

    initial commit

---
+++ src/main/java/com/gestone/gestone_api/GestoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestone.gestone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestone/gestone_api/GestoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestone.gestone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
